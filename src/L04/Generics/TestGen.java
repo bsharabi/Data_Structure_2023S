87d3f52057931ee8e4f404bdb0eed6c9e930fc93
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2023-08-20 22:20:37 +0300

    L04

---
+++ src/L04/Generics/TestGen.java
@@ -0,0 +1,27 @@
+package L04.Generics;
+
+import java.util.Arrays;
+
+public class TestGen {
+
+    public static void main(String[] args) {
+
+        int a = 9, b = 10;
+        float a1 = 9.5f, b1 = 10.5f;
+
+
+        int ans = sum(a, b);
+
+        float ans1 = sum(a1, b1);
+//        sum(5.5,5.6);
+//         sum(5,5);
+
+    }
+
+    private static int sum(int a, int b) {
+        return a + b;
+    }
+    private static float sum(float a, float b) {
+        return a + b;
+    }
+}
